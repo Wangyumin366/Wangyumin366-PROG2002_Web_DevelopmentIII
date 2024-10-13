b0f3d6e28f1c52b7fe1ac5ecae92f0c5881665ae
Wangyumin366 <145888531+Wangyumin366@users.noreply.github.com>
2024-10-13 23:14:09 +0800

    crate component and router and service

--- A2-client/src/app/app-routing.module.ts
+++ A2-client/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { SearchComponent } from "./search/search.component";
+import { FundraiserComponent } from "./fundraiser/fundraiser.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  },
+  {
+    path: 'fundraiser/:id',
+    component: FundraiserComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
