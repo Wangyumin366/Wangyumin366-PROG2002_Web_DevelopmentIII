cdb378f712481cf18250e6b7363934a405100104
Wangyumin366 <145888531+Wangyumin366@users.noreply.github.com>
2024-10-13 22:17:36 +0800

    create donation table and insert 10 records

--- A2-api/crowdfunding_db.sql
+++ A2-api/crowdfunding_db.sql
@@ -65,19 +65,33 @@ CREATE TABLE `fundraiser` (
 -- Dumping data for table `fundraiser`
 --
 
-LOCK TABLES `fundraiser` WRITE;
-/*!40000 ALTER TABLE `fundraiser` DISABLE KEYS */;
-INSERT INTO `fundraiser` VALUES (1,'Martha Stewart','Cancer Treatment for a Local Resident',25000,19000,'Philadelphia',2,1),(2,'EcoFuture','Plastic-Free Oceans Initiative',50000,22000,'San Diego',3,1),(3,'Creative Minds','Public Art Installation',15000,8000,'Portland',4,1),(4,'Save the Paws','Spay and Neuter Campaign',12000,5000,'Denver',5,1),(5,'Bright Horizons','STEM Workshops for Girls',8000,3000,'San Jose',1,1),(6,'Healing Hands','Support for Rare Disease Research',30000,15000,'Orlando',2,1),(7,'Green Earth','Reforestation in National Parks',40000,18000,'Phoenix',3,1);
-/*!40000 ALTER TABLE `fundraiser` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- ----------------------------
+-- Table structure for donation
+-- ----------------------------
+DROP TABLE IF EXISTS `donation`;
+CREATE TABLE `donation`  (
+  `DONATION_ID` int NOT NULL AUTO_INCREMENT,
+  `DATE` datetime NULL DEFAULT NULL,
+  `AMOUNT` decimal(10, 2) NULL DEFAULT NULL,
+  `GIVER` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `FUNDRAISER_ID` int NULL DEFAULT NULL,
+  PRIMARY KEY (`DONATION_ID`) USING BTREE,
+  INDEX `donation_fundraiser_fk`(`FUNDRAISER_ID` ASC) USING BTREE,
+  CONSTRAINT `donation_fundraiser_fk` FOREIGN KEY (`FUNDRAISER_ID`) REFERENCES `fundraiser` (`FUNDRAISER_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- ----------------------------
+-- Records of donation
+-- ----------------------------
+INSERT INTO `donation` VALUES (1, '2024-09-21 14:45:10', 500.00, 'Sarah Walker', 1);
+INSERT INTO `donation` VALUES (2, '2024-09-22 11:25:34', 1000.00, 'Ethan Scott', 2);
+INSERT INTO `donation` VALUES (3, '2024-09-23 09:50:50', 250.00, 'Sophia Lewis', 3);
+INSERT INTO `donation` VALUES (4, '2024-09-24 15:40:12', 750.00, 'Daniel Harris', 4);
+INSERT INTO `donation` VALUES (5, '2024-09-25 10:10:05', 1200.00, 'Grace White', 5);
+INSERT INTO `donation` VALUES (6, '2024-09-26 12:30:25', 300.00, 'Jacob Hall', 6);
+INSERT INTO `donation` VALUES (7, '2024-09-27 13:55:47', 400.00, 'Mia Green', 7);
+INSERT INTO `donation` VALUES (8, '2024-09-28 17:35:56', 600.00, 'Aiden Young', 2);
+INSERT INTO `donation` VALUES (9, '2024-09-29 08:05:14', 900.00, 'Lily King', 1);
+INSERT INTO `donation` VALUES (10, '2024-09-30 16:22:48', 850.00, 'Lucas Adams', 3);
 
 -- Dump completed on 2024-10-02  0:38:28
